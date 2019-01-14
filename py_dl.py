a20edb744ec044de6932a8b8628dd99e69293767
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-01-14 23:11:40 +0330

    added auto pep 8

--- py_dl.py
+++ py_dl.py
@@ -1,4 +1,4 @@
-#! /etc/python3
+#!/usr/bin/python3
 
 import os
 import subprocess as sp
@@ -24,7 +24,7 @@ def is_downloadable(url):
     return True
 
 
-#def one_dl(pre_url , every_link):
+# def one_dl(pre_url , every_link):
 #    print ("downloading:\n",pre_url+every_link)
 #    try:
 #        wg_comand  = "wget -c "+pre_url+every_link
@@ -33,106 +33,127 @@ def is_downloadable(url):
 #       print("error downloading file")
 
 
-def add_single(inp_url = None):
-    if(inp_url == None): inp_url = input("pls enter a url\n")
+def add_single(inp_url=None):
+    if(inp_url == None):
+        inp_url = input("pls enter a url\n")
     try:
-        list = open('list.txt','a')
-        list.write(inp_url+ "\n")
-        print(inp_url ," added to list")
-    except: print("error in appending to list    ")
-
-
+        list = open('list.txt', 'a')
+        list.write(inp_url + "\n")
+        print(inp_url, " added to list")
+    except:
+        print("error in appending to list    ")
 
 
 def add_multi_single():
     print("now enter links, separated by enter")
-    while(1==1):
+    while(1 == 1):
         inp_url = input("pls enter a url,done = d\n")
-        if inp_url.lower() in ["done","d","end","e","exit"] : break;
+        if inp_url.lower() in ["done", "d", "end", "e", "exit"]:
+            break
         add_single(inp_url)
 
 
-
 def auto_add():
-    regex = r"[Ss]\d{1,2}[Ee](\d{1,2})";
+    regex = r"[Ss]\d{1,2}[Ee](\d{1,2})"
     main_url = input("pls enter first episode link here:\n")
     m = re.search(regex, main_url)
-    if m == None :
+    if m == None:
         print("no serial found")
-    else :
+    else:
         all = m.group()
-        first_ep =   int(all[4:6])
-        n = int (input("last episode?: "))
-        for i in range(n+1 - first_ep ):
+        first_ep = int(all[4:6])
+        n = int(input("last episode?: "))
+        for i in range(n+1 - first_ep):
             final = main_url
-            episode = str (first_ep + i)
-            if int(episode) < 10 : episode = "0" + episode
+            episode = str(first_ep + i)
+            if int(episode) < 10:
+                episode = "0" + episode
             episode = all[0:4] + episode
             final = re.sub(regex, episode, final)
             add_single(final)
 
 
-
 def add_batch():
     inp_url = input("pls enter a url\n")
     print("parsing html...")
-    try :
+    try:
         test = urllib.request.urlopen(inp_url).read()
         page = requests.get(inp_url)
         bs = BeautifulSoup(page.content, features='lxml')
-    except :
+    except:
         print("error parsing html")
         return
 
     keyword = input("pls enter keyword to search: (a for nothing) ")
-    if keyword == "a" or keyword == "" : keyword = "";
+    if keyword == "a" or keyword == "":
+        keyword = ""
 
     OPT = input("page have full link or no? (f or n)")
     pre_url = inp_url
-    if OPT in 'fy ' : pre_url = "";
-
+    if OPT in 'fy ':
+        pre_url = ""
 
     for link in bs.findAll('a'):
         every_link = link.get('href')
         if keyword in every_link:
-            add_single(pre_url+every_link);
+            add_single(pre_url+every_link)
 
 
 def start_wget():
     print("starting wget . .. \n downloading to  ~/Downloads/dl_py/")
-    #-O ~/Downloads/dl_py/
-    aria_command =  "aria2c -i list.txt  -c -d ~/Downloads/aria_dl/ -j1 "
-    wg_command  = "wget -c -i list.txt -P ~/Downloads/dl_py/"
+    # -O ~/Downloads/dl_py/
+    aria_command = "aria2c -i list.txt  -c -d ~/Downloads/aria_dl/ -j1 "
+    wg_command = "wget -c -i list.txt -P ~/Downloads/dl_py/"
     os.system(wg_command)
+
 
 def print_list():
     print()
-    os.system("cat list.txt");
+    os.system("cat list.txt")
+
 
 def edit_list():
-    os.system("nano list.txt");
+    os.system("nano list.txt")
+
+
+def shutdown():
+    start_wget()
+    os.system("shutdown")
 
 # - - -- main program - - - -
 
-while (1==1):
+
+while (1 == 1):
 
     Command = input("\nwhat do you want to do?\nadd html Batch = b \nSingle file = s\nmulti single = bs  \nContinue Download = c\nPrint queue = p\nEdit queue = e\nAuto serial find = a\n")
     command = Command.lower()
 
-    if command in ["continue" ,"download" , "d" , "c" ] : start_wget();
+    if command in ["continue", "download", "d", "c"]:
+        start_wget()
 
-    elif command in ["single", "s"] : add_single()
+    elif command in ["shut", "cs", "csh", "shutd"]:
+        shutdown()
 
-    elif command in ["a","auto","ad"]: auto_add()
+    elif command in ["single", "s"]:
+        add_single()
 
-    elif command in ["bs","multi","ms"]: add_multi_single()
+    elif command in ["a", "auto", "ad"]:
+        auto_add()
 
-    elif command in ["batch" , "b"] : add_batch()
+    elif command in ["bs", "multi", "ms"]:
+        add_multi_single()
 
-    elif command in ["print" , "p"] : print_list()
+    elif command in ["batch", "b"]:
+        add_batch()
 
-    elif command in ["edit" , "e"] : edit_list()
+    elif command in ["print", "p"]:
+        print_list()
 
-    elif command in ["quit" , "q" , 'exit', "x", "ex"] : exit()
+    elif command in ["edit", "e"]:
+        edit_list()
 
-    else : os.system(Command)
+    elif command in ["quit", "q", 'exit', "x", "ex"]:
+        exit()
+
+    else:
+        os.system(Command)
